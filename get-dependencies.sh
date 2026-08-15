6644c4152b05fae30bbc7cf0c91f6c3a21982db1
Link4Electronics <link4electronics@gmail.com>
2026-08-15 06:04:05 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,13 +12,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
 	package=xemu-git
 else
