b1ebe84f02a956f17061b76bd0859bb817a9901e
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:38:14 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/xemu.svg
 export DESKTOP=/usr/share/applications/xemu.desktop
 export STARTUPWMCLASS=xemu
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
@@ -20,7 +18,3 @@ quick-sharun /usr/bin/xemu
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
